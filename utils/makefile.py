bcc2452b9bc693c9dc0e8fb764f9cf4b64304c3b
Sonika Garg <sonikag@cs.utexas.edu>
2018-04-09 17:11:37 -0500

    added a script to create files of variable lengths and random data

---
+++ utils/makefile.py
@@ -0,0 +1,28 @@
+'''
+This script creates files of data for us to run our programs on
+Authors: Madeline Stager and Sonika Garg 
+'''
+
+import random
+
+MIN_VALUE = -1000000
+MAX_VALUE = 1000000
+
+
+#method to create a file (named fileName) of numValues length and random data
+def createFile(numValues, fileName):
+    file = open(fileName, "w")
+    
+    for i in range(numValues):
+        num = random.randint(MIN_VALUE, MAX_VALUE)
+        file.write(str(num) + " ")
+    
+    file.write("\n")
+    file.close()
+
+
+
+createFile(6, "prefixTest1.txt")
+
+    
+

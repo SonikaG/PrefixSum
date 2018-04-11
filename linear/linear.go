4436351bbf2e948834f96ba6adddd409af4bb44c
Sonika Garg <sonikag@cs.utexas.edu>
2018-04-11 16:13:51 -0500

    added file reading and timing to the go file

--- linear/linear.go
+++ linear/linear.go
@@ -1,6 +1,11 @@
 package main
 import "fmt"
+import "io/ioutil"
+import "strings"
+import "strconv"
+import "time"
 
+//prefix sum method
 func prefixSum (input []int64, input_size int) ([]int64) {
     result := make([]int64, input_size)
     for i:= 0; i < input_size; i++{
@@ -13,12 +18,44 @@ func prefixSum (input []int64, input_size int) ([]int64) {
     return result;
 }
 
+//function to read the contents of the file
+func readFile(input string) ([]int64){
+  whole, err := ioutil.ReadFile(input)
+  if err != nil {
+    fmt.Printf("OH NO, couldn't read file")
+    panic(err)
+  }
+  lines := strings.Split(string(whole), "\n")
+
+  num_points, err := strconv.Atoi(lines[0])
+  //fmt.Println(num_points)
+  values := strings.Split(lines[1], " ")
+  numbers := make([]int64, num_points)
+  for i := 0; i < len(values)-1; i++{
+    val, err := strconv.ParseInt(values[i], 10, 64)
+    if(err != nil){
+      panic(err)
+    }
+    numbers[i] = val
+  }
+  //fmt.Println(numbers)
+  return numbers
+
+}
+
+//main method 
 func main (){
-    testOne := []int64{1, 2, 3, 4, 5}
+    /*testOne := []int64{1, 2, 3, 4, 5}
     result := prefixSum(testOne, len(testOne))
     fmt.Println(result)
 
     testTwo := []int64{-2, 16, 4, 1, 7, -3, 8, 3, 0, -6}
     resultTwo := prefixSum(testTwo, len(testTwo))
-    fmt.Println(resultTwo)
+    fmt.Println(resultTwo)*/
+    numbers := readFile("../utils/prefixTest1.txt")
+    start := time.Now()
+    result := prefixSum(numbers, len(numbers))
+    elapsed := time.Since(start)
+    fmt.Printf("elapsed time: %.9f\n", elapsed.Seconds())
+    fmt.Println(result)
 }
